e640c8468f1f1b0f304a4806c1399a6000b87382
plextor15 <75131243+plextor15@users.noreply.github.com>
2021-04-12 19:06:46 +0200

    Almost there

--- ASD2gr3_PorembinskiA_heapsort.cpp
+++ ASD2gr3_PorembinskiA_heapsort.cpp
@@ -1,8 +1,35 @@
 /*
-* 
+*
 *	https://eduinf.waw.pl/inf/alg/003_sort/0015.php
 */
 #include <iostream>
+
+void kopcowanie(long *dane, int start, int end) {
+	long tmp;
+	int a, b;
+
+	for (int i = start; i <= end; i++) {
+		tmp = dane[i];
+		a = i;
+		b = a / 2;
+
+		while ((tmp > dane[b])&&(b > 0)) {
+			dane[a] = dane[b];
+			a = b;
+			b = a / 2;
+		}
+		dane[a] = tmp;
+	}
+
+	return;
+}
+
+void zamiana(long *dane, int ktory, int koniec) {
+	long tmp = dane[koniec];
+	dane[koniec] = dane[ktory];
+	dane[ktory] = tmp;
+	return;
+}
 
 int ileTestow = 0;
 
@@ -11,8 +38,8 @@ int main() {
 	int ileDanych = 0;
 
 	//zmienne do petli sortowania
-	int a, b;
-	long tmp = 0;
+	//int a, b;
+	//long tmp = 0;
 
 	std::cin >> ileTestow;
 	while (ileTestow > 0) {
@@ -23,33 +50,55 @@ int main() {
 		dane[0] = 0;	//nie bedzie dotykana
 		for (int j = 1; j < ileDanych + 1; j++) {
 			std::cin >> dane[j];
-			std::cout << dane[j] << ' '; //DEBUG ONLY!!
+			//std::cout << dane[j] << ' '; //DEBUG ONLY!!
 		}
+		std::cout << "\n----------------------\n"; //DEBUG ONLY!!
+
+		////DEBUG ONLY!!
+		//std::cout << '\n';
+		//for (int i = 0; i < ileDanych+1; i++) {
+		//	std::cout << dane[i] << ' ';
+		//} std::cout << "\nWaskol\n";
+		////DEBUG ONLY!!
 
 		//tworzenie kopca
-		for (int i = 0; i < ileDanych; i++) {
-			tmp = dane[i];
-			a = i;
-			b = a / 2;
+		kopcowanie(dane, 1, ileDanych);
 
-			while ((tmp > dane[b])&&(b > 0)) {
-				dane[a] = dane[b];
-				a = b;
-				b = a / 2;
-			}
-			dane[a] = tmp;
-		}
-
-		//DEBUG ONLY!!
-		std::cout << '\n';
-		for (int i = 0; i < ileDanych; i++) {
+		//wypisanie kopca
+		for (int i = 1; i < ileDanych + 1; i++) {
 			std::cout << dane[i] << ' ';
 		}
-		//DEBUG ONLY!!
+		std::cout << '\n';
+		
+		//wybieranie wierzcholka
+		for (int i = 0; i < ileDanych-1; i++){ //debug
+			zamiana(dane, 1, ileDanych-i);
+			kopcowanie(dane, 1, ileDanych-1-i);
+
+			if (1 < ileDanych - i) {
+				for (int j = 1; j < ileDanych - i; j++) {
+					std::cout << dane[j] << ' ';
+				}
+			}
+			std::cout << '\n';
+		}
+
+		//wypisanie posortowanych
+		for (int i = 1; i < ileDanych + 1; i++) {
+			std::cout << dane[i] << ' ';
+		}
+		std::cout << '\n';
+
+		////DEBUG ONLY!!
+		//std::cout << '\n';
+		//for (int i = 0; i < ileDanych+1; i++) {
+		//	std::cout << dane[i] << ' ';
+		//}//DEBUG ONLY!!
 
 		delete[] dane;
-		dane = NULL;
+		dane = NULL; //debug
 		ileTestow -= 1;
+		std::cout << '\n'; //pusta linia miedzy przypadkami
 	}
 
 	return 0;
