59b956673f44ee5c5377e38587fe2310861e58a8
plextor15 <75131243+plextor15@users.noreply.github.com>
2022-01-04 15:30:11 +0100

    na symulacje

--- autostrada.cpp
+++ autostrada.cpp
@@ -5,16 +5,16 @@
 #include <stdlib.h>
 #include <math.h>
 
-const int LiczbaSymulacji = 10; //finalnie 2000
+const int LiczbaSymulacji = 1000; //finalnie 2000
 const double pi = 3.14159265358979323846;
-double CalkowityCzasJazdy = 0;
+double CalkowityCzasJazdy;
 
 double Los01()
 {
 	return (1.0 * rand()) / RAND_MAX;
 }
 
-double LosDyskretna(int max, double tab[])
+int LosDyskretna(int max, double tab[]) //nie double
 {
 	double r;
 	int j;
@@ -35,7 +35,7 @@ double LosDyskretna(int max, double tab[])
 		}
 	} while (check);
 
-	return;
+	return -1;
 }
 
 double N01_BM(double my, double sigma)	//rozkaad N(0,1) – metoda Boxa-Millera
@@ -54,6 +54,23 @@ double N01_BM(double my, double sigma)	//rozkaad N(0,1) – metoda Boxa-Millera
 	return (z1 * sigma) + my;
 }
 
+double RozkladTrojkatny(double x_od, double x_do)
+{
+	double tmp;
+	double x = Los01();
+	
+	if (x <= 0.5)
+	{
+		tmp = sqrt(2 * x);
+	}
+	else
+	{
+		tmp = 2 - sqrt(2 * (1 - x));
+	}
+
+	return (x_do - x_od) * tmp + x_od;
+}
+
 int main()	//wszystkie czasy w minutach, odleglosci w kilometrach
 {
 	srand(time(NULL));
@@ -63,20 +80,47 @@ int main()	//wszystkie czasy w minutach, odleglosci w kilometrach
 
 	const int ilePoryDnia = 3;
 	double PrawdopodPoraDnia[ilePoryDnia] = { 0.29169, 0.54171, 0.16668 };
+	double SzybkoscPoraDnia_miasto[ilePoryDnia] = { 1.2, 1, 0.6 };
+	const double PredkoscWMiescie = 0.833;	// 50km/h w km/min
 	// 1h/24 = 0.04167
 	// noc 23-6 = 7h 60km/h - 1.2
 	// dzien = 13h 50km/h - 1
 	// ok 8 i ok 16 = 4h 30km/h - 0.6
+
 	int PoraWyjazdu, PoraWjazdu;
 	double CzasWyjazdu, CzasWjazdu;
-	double SzybkoscPoraDnia_miasto[ilePoryDnia] = { 1.2, 1, 0.6 };
-	const double PredkoscWMiescie = 0.833;	// 50km/h w km/min
+	
 	double WielkoscKorkow;
 	const double maxKorek = 0.1;	//np: 5km/h dla 50km/h
 	const double KmDoAutostrady = 10.5; //odleglosc od wjazdu/wyjazdu z autostrady do miejsca docelowego w miescie
 
-	const double maxCzasNaBramce = 30;
+
+	const int ileRodzajePogody = 4; //bardzo dobra, srednia, zla, fatalna
+	double PrawdopodRodzajPogody[ileRodzajePogody] = { 0.7, 0.5, 0.1, 0.02 };
+	double PredkoscRodzajPogody[ileRodzajePogody] = { 1, 0.9, 0.7, 0.5 };
+	int RodzajPogody;
+	double PredkoscPogoda;
+
+	const double PredkoscNaAutostradzie = 2.33;
+	const int ileStylJazdy = 2; //szybka, ekonomiczna
+	double PrawdopodStylJazdy[ileStylJazdy] = { 0.6, 0.4 };
+	double PredkoscStylJazdy[ileStylJazdy] = { 1, 0.8 };
+	int StylJazdy;
+	double PredkoscStylu;
+
+	double iloscTirow;	// <0-1>
+	const double minPredkoscBoTiry = 0.7; // % predkosci jakby ich nie bylo
+	double PredkoscBoTiry; // to nie jest prekosc tirow, tylko % max predkosci jakby ich nie bylo
+
+	double PredkoscWynikowa; // % max predkosci jaka moze byc
+	double PredkoscJazdyAutostrada;
+	double CzasJazdyAutostrada;
+
+
+	const int ileBramek = 4;
+	const double maxCzasNaBramce = 11;
 	double CzasNaBramce;
+	double CalkowityCzasBramek;
 
 
 	int ilePostojow;
@@ -91,86 +135,76 @@ int main()	//wszystkie czasy w minutach, odleglosci w kilometrach
 	double PrawdopodobZjedzeniaCzegos = 0.4;
 	double myZjedzenia = 3, sigmaZjedzenia = 0.65; //N(0,2) Boxa-Mullera
 
-	const int ileBramek = 4;
-
 
 
 	double GestoscRuchu;
 	double maxGestoscRuchu = 0.5; //polowa mozliwej predkosci
 
 	//do dyskretnej
-	int j;
-	double r;
-
+	//int j;
+	//double r;
 	//metoda Boxa-Millera
-	double u1, u2, R, Th, z1, z2;
+	//double u1, u2, R, Th, z1, z2;
 
 	std::ofstream wynikowy;
 	wynikowy.open("wynikowy.txt");
 
 	for (size_t L = 0; L < LiczbaSymulacji; L++)	//petla przebiegu jednej symulacji
 	{
-		//WYJAZD Z MIASTA
-		//losowanie dyskretne pory dnia
+		CalkowityCzasJazdy = 0;
 
-		//do
-		//{
-		//	r = Los01();
-		//	j = rand() % ilePoryDnia;
-		//	if (r <= PrawdopodPoraDnia[j])
-		//	{
-		//		PoraWyjazdu = j;	//max tyle postojow ile prawdopodob w tabeli
-		//		check = false;
-		//	}
-		//	else
-		//	{
-		//		check = true;
-		//	}
-		//} while (check);
+		//WYJAZD Z MIASTA
 		PoraWyjazdu = LosDyskretna(ilePoryDnia, PrawdopodPoraDnia);
 
 		WielkoscKorkow = Los01();
 		CzasNaBramce = Los01() * maxCzasNaBramce;
 
-		CzasWyjazdu = ( (-(1-maxKorek)*WielkoscKorkow+1) * SzybkoscPoraDnia_miasto[PoraWyjazdu]) * KmDoAutostrady + CzasNaBramce;
+		CzasWyjazdu = ((-(1 - maxKorek) * WielkoscKorkow + 1) * SzybkoscPoraDnia_miasto[PoraWyjazdu]) * KmDoAutostrady + CzasNaBramce;
 
 		//JAZDA PO AUTOSTRADZIE
-		//tiry
+		RodzajPogody = LosDyskretna(ileRodzajePogody, PrawdopodRodzajPogody);
+		PredkoscPogoda = PredkoscRodzajPogody[RodzajPogody];
+		StylJazdy = LosDyskretna(ileStylJazdy, PrawdopodStylJazdy);
+		PredkoscStylu = PredkoscStylJazdy[StylJazdy];
+		iloscTirow = Los01();
+		PredkoscBoTiry = 1 - (1-minPredkoscBoTiry) * iloscTirow;
 
-		//pogoda
+		/*if (PredkoscPogoda > PredkoscStylu)
+		{
+			PredkoscPogoda = PredkoscStylu;
+		}
 
-		//styl jazdy
+		if (PredkoscPogoda > PredkoscBoTiry)
+		{
+			PredkoscPogoda = PredkoscBoTiry;
+		}*/
+		if (true)
+		{
 
+		}
+		
+		CzasJazdyAutostrada = 0 * 0;
 
 		//BRAMKI DO OPLAT
-
+		tmp = 0;
+		CalkowityCzasBramek = 0;
+		for (size_t i = 0; i < ileBramek; i++)
+		{
+			tmp = N01_BM(0, 1.2);
+			tmp = abs(tmp);
+			tmp *= maxCzasNaBramce;
+			CalkowityCzasBramek += tmp;
+			//CzasNaBramce += 0.2; //czas na obsluzenie nas
+		}
 
 
 		//POSTOJE
 		PostojeCzasCalkowity = 0;
-		//ilosc postojow losowana z rozkladu zmiennej dyskretnej
-		
-		//do
-		//{
-		//	r = Los01();
-		//	j = rand() % maxPostojow;
-		//	if (r <= TabProbPostojow[j])
-		//	{
-		//		ilePostojow = j;	//max tyle postojow ile prawdopodob w tabeli
-		//		check = false;
-		//	}
-		//	else
-		//	{
-		//		check = true;
-		//	}
-		//} while (check);
 		ilePostojow = LosDyskretna(maxPostojow, TabProbPostojow);
-
-		//co bedzie robione na kazdym postoju
-		for (size_t i = 0; i < ilePostojow; i++)
+		
+		for (size_t i = 0; i < ilePostojow; i++)//co bedzie robione na kazdym postoju
 		{
-			//wyjscie do toalety - TAK/NIE
-			if (Los01() > 0.5)
+			if (Los01() > 0.5)//wyjscie do toalety - TAK/NIE
 			{
 				czyToaleta = true;
 			}
@@ -180,8 +214,7 @@ int main()	//wszystkie czasy w minutach, odleglosci w kilometrach
 			}
 			//std::cout << "Toaleta: " << czyToaleta << "\n";	//FOR DEBUG ONLY!!! 
 
-			//czy zjedzenie czegos
-			if (Los01() > PrawdopodobZjedzeniaCzegos)
+			if (Los01() > PrawdopodobZjedzeniaCzegos)//czy zjedzenie czegos
 			{
 				CzasZjedzenia = N01_BM(myZjedzenia, sigmaZjedzenia);
 			}
@@ -200,17 +233,16 @@ int main()	//wszystkie czasy w minutach, odleglosci w kilometrach
 			}
 			PostojeCzasCalkowity += 2;	//np: zaparkowanie
 			//std::cout << "Czas zjedzenia: " << CzasZjedzenia << "\n";	//FOR DEBUG ONLY!!! 
-
-
-			//WJAZD Z MIASTA
-			//pora dnia wjazdu
-			PoraWjazdu = (PoraWyjazdu + 2) % ilePoryDnia;
-
-
 		}
 		//std::cout << "calkowity czas: " << PostojeCzasCalkowity << " w tym - " << ilePostojow << "\n\n\n";	//FOR DEBUG ONLY!!!
+		
+		//WJAZD Z MIASTA
+		PoraWjazdu = (PoraWyjazdu + 2) % ilePoryDnia; //finalnie moze byc inaczej liczone
+		GestoscRuchu = RozkladTrojkatny(0, 1);
+		//CzasWjazdu = ((-(1 - maxGestoscRuchu) * GestoscRuchu + 1) * SzybkoscPoraDnia_miasto[PoraWjazdu]) * KmDoAutostrady;
 
-
+		//CalkowityCzasJazdy = CzasWyjazdu + CalkowityCzasBramek + PostojeCzasCalkowity + CzasWjazdu;
+		//std::cout << "\n" << CalkowityCzasJazdy << "\n"; //FOR DEBUG ONLY!!!
 	}
 
 
